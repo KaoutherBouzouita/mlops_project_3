24a12341a0556320b857b70f6de20ff63bd2e35f
KaoutherBz <k.bouzouita@instadeep.com>
2023-07-20 18:15:41 +0100

    slice_averages WIP

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -1,11 +1,11 @@
 import os
+import pickle
 
 import pytest
 import pandas as pd
 
-from . import DATA_DIR
 from .data import process_data
-from .model import train_model
+from .model import train_model, compute_model_metrics, inference
 
 
 @pytest.fixture
@@ -28,26 +28,82 @@ def cat_features():
 
 
 @pytest.fixture
-def X(df, cat_features):
+def X_train(df, cat_features):
     X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
     return X
 
 
 @pytest.fixture
-def y(df, cat_features):
+def y_train(df, cat_features):
     X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
     return y
 
 
-def test_X(X):
-    assert X.shape == (32561, 108)
+@pytest.fixture
+def encoder(df, cat_features):
+    X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
+    return encoder
 
 
-def test_y(y):
-    assert y.shape == (32561,)
+@pytest.fixture
+def lb(df, cat_features):
+    X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
+    return lb
 
 
-def test_train_model(df, X, y):
+@pytest.fixture
+def X_test(df, cat_features, encoder, lb):
+    X_test, y_test, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=False,
+                                               encoder=encoder, lb=lb)
+    return X_test
+
+
+@pytest.fixture
+def y_test(df, cat_features, encoder, lb):
+    X_test, y_test, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=False,
+                                               encoder=encoder, lb=lb)
+    return y_test
+
+
+@pytest.fixture
+def model():
+    return pickle.load(open(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..',
+                                                          'model', 'model.sav')), 'rb'))
+
+
+@pytest.fixture
+def y_pred(model, X_test):
+    return inference(model, X_test)
+
+
+def test_X(X_train):
+    assert X_train.shape == (32561, 108)
+
+
+def test_y(y_train):
+    assert y_train.shape == (32561,)
+
+
+def test_train_model(df, X_train, y_train):
     assert df.shape == (32561, 15)
-    model = train_model(X_train=X, y_train=y)
+    model = train_model(X_train=X_train, y_train=y_train)
     assert len(model.classes_) == 2
+
+
+def test_slice_averages(cat_features, X_test, y_test, df, model):
+    """ Function for calculating descriptive stats on slices of the Adult dataset."""
+    for cat in cat_features:  # cat in ['workclass', 'education', 'marital-status', 'occupation',...]
+        for value in df[cat].unique():
+            # Features values for the selected value
+            y_test_cat = y_test[df[df[cat] == value].index]
+            X_test_cat = X_test[df[df[cat] == value].index]
+            # Run inference
+            y_pred_cat = inference(model, X_test_cat)
+            # Model metrics for each category
+            precision, recall, fbeta = compute_model_metrics(y_test_cat, y_pred_cat)
+
+            print(f"Feature: {cat} - Value {value}")
+            print(f"Precision: {precision:.4f}")
+            print(f"Recall: {recall:.4f}")
+            print(f"Fbeta: {fbeta:.4f}")
+        print()
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -9,11 +9,31 @@ from ml.data import process_data
 import pickle
 from sklearn.tree import DecisionTreeClassifier
 
-from . import DATA_DIR
-from starter.starter.ml.model import train_model, inference, compute_model_metrics
+from ml.model import train_model, inference, compute_model_metrics
+
+
+
+def slice_averages(cat_features, X_test, y_test, df, model):
+    """ Function for calculating descriptive stats on slices of the Adult dataset."""
+    for cat in cat_features:  # cat in ['workclass', 'education', 'marital-status', 'occupation',...]
+        for value in df[cat].unique():
+            # Features values for the selected value
+            y_test_cat = y_test[df[df[df[cat] == value].index]["salary"]]
+            X_test_cat = X_test[df[df[df[cat] == value].index].drop["salary"]]
+            # Run inference
+            y_pred_cat = inference(model, X_test_cat)
+            # Model metrics for each category
+            precision, recall, fbeta = compute_model_metrics(y_test_cat, y_pred_cat)
+
+            print(f"Feature: {cat} - Value {value}")
+            print(f"Precision: {precision:.4f}")
+            print(f"Recall: {recall:.4f}")
+            print(f"Fbeta: {fbeta:.4f}")
+        print()
+
 
 # Add code to load in the data.
-data = pd.read_csv(os.path.join(os.path.abspath(DATA_DIR), "census.csv"))
+data = pd.read_csv(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', "census.csv")))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -49,6 +69,14 @@ pickle.dump(model, open(filename, 'wb'))
 # Run inference
 y_pred = inference(model, X_test)
 
+print(f"Y test: {y_test}")
+print(f"Y pred: {y_pred}")
+
 # Compute model metrics
 precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
 
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("Fbeta: ", fbeta)
+
+slice_averages(cat_features, X_test, y_test, data, model)
