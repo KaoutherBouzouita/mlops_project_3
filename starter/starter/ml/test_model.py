0e9a2d6928232b7109d429e08a8b0ac3e8959f0f
KaoutherBz <k.bouzouita@instadeep.com>
2023-07-20 10:01:57 +0100

    Fix path to model and data modules

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -1,8 +1,8 @@
 import pytest
 import pandas as pd
 
-from starter.starter.ml.data import process_data
-from starter.starter.ml.model import train_model
+from .data import process_data
+from .model import train_model
 
 
 @pytest.fixture
