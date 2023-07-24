03e5affe0f82f72d487e64a0747a5050e2952fd5
KaoutherBz <k.bouzouita@instadeep.com>
2023-07-24 15:22:52 +0100

    Fix: test_model imports

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -4,9 +4,9 @@ import pytest
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from .data import process_data
-from .model import train_model, inference
-from starter.starter.ml.train_model import load_model
+import starter.starter.ml.data as data
+import starter.starter.ml.model as model
+import starter.starter.ml.train_model as train_model
 
 
 @pytest.fixture
@@ -42,50 +42,50 @@ def cat_features():
 
 @pytest.fixture
 def X_train(df_train, cat_features):
-    X, y, encoder, lb = process_data(df_train, categorical_features=cat_features, label="salary", training=True)
+    X, y, encoder, lb = data.process_data(df_train, categorical_features=cat_features, label="salary", training=True)
     return X
 
 
 @pytest.fixture
 def y_train(df_train, cat_features):
-    X, y, encoder, lb = process_data(df_train, categorical_features=cat_features, label="salary", training=True)
+    X, y, encoder, lb = data.process_data(df_train, categorical_features=cat_features, label="salary", training=True)
     return y
 
 
 @pytest.fixture
 def encoder(df_train, cat_features):
-    X, y, encoder, lb = process_data(df_train, categorical_features=cat_features, label="salary", training=True)
+    X, y, encoder, lb = data.process_data(df_train, categorical_features=cat_features, label="salary", training=True)
     return encoder
 
 
 @pytest.fixture
 def lb(df_train, cat_features):
-    X, y, encoder, lb = process_data(df_train, categorical_features=cat_features, label="salary", training=True)
+    X, y, encoder, lb = data.process_data(df_train, categorical_features=cat_features, label="salary", training=True)
     return lb
 
 
 @pytest.fixture
 def X_test(df_test, cat_features, encoder, lb):
-    X_test, y_test, encoder, lb = process_data(df_test, categorical_features=cat_features, label="salary", training=False,
+    X_test, y_test, encoder, lb = data.process_data(df_test, categorical_features=cat_features, label="salary", training=False,
                                                encoder=encoder, lb=lb)
     return X_test
 
 
 @pytest.fixture
 def y_test(df_test, cat_features, encoder, lb):
-    X_test, y_test, encoder, lb = process_data(df_test, categorical_features=cat_features, label="salary", training=False,
+    X_test, y_test, encoder, lb = data.process_data(df_test, categorical_features=cat_features, label="salary", training=False,
                                                encoder=encoder, lb=lb)
     return y_test
 
 
 @pytest.fixture
-def model():
-    return load_model(model_name='model.sav')
+def trained_model():
+    return model.load_model(model_name='model.sav')
 
 
 @pytest.fixture
-def y_pred(model, X_test):
-    return inference(model, X_test)
+def y_pred(trained_model, X_test):
+    return model.inference(model, X_test)
 
 
 def test_X(X_train):
@@ -98,7 +98,7 @@ def test_y(y_train):
 
 def test_train_model(df, X_train, y_train):
     assert df.shape == (32561, 15)
-    model = train_model(X_train=X_train, y_train=y_train)
-    assert len(model.classes_) == 2
+    trained_model = model.train_model(X_train=X_train, y_train=y_train)
+    assert len(trained_model.classes_) == 2
 
 
